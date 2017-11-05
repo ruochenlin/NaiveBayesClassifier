6ef2993e5acd8a2ade00818657da6e1a09450046
Ruochen Lin <ruochenlin@outlook.com>
2017-11-04 20:33:59 -0500

    Added Source

--- src/DataPrep.hpp
+++ src/DataPrep.hpp
@@ -1,7 +1,7 @@
 #ifndef DATAPREP_HPP_
 #define DATAPREP_HPP_
-#include<iostream>
-#include<fstream>
+#include <iostream>
+#include <fstream>
 #include <string>
 #include <cstdlib>
 using namespace std;
@@ -15,7 +15,7 @@ void dataPrep(string const &trainFile, string const &testFile)
 		"attrCount=`grep -c attribute " << trainFile << " `" << endl <<
 		"entryCount=$(($((lineCount)) - 2 - $((attrCount)))) " << endl <<
 		"echo \"$attrCount $entryCount\"> attr.tmp " << endl <<
-		"grep attribute " << trainFile << "  | sed -e 's/{/ nominal /g' | sed -e 's/numeric/real/g' | sed -e 's/}//g' | sed -e \"s/' / /g\" | awk -F=\"attribute\" '{print }' | awk -F \"'\" '{print $2}' | sed -e 's/,/ /g' | sed -e 's/  */ /g' >> attr.tmp " << endl <<
+		"grep attribute " << trainFile << "  | sed -e 's/{/ nominal /g' | sed -e 's/numeric/real/g' | sed -e 's/}//g' | sed -e \"s/' / /g\" | awk -F \"'\" '{print $2}' | sed -e 's/,/ /g' | sed -e 's/  */ /g' >> attr.tmp " << endl <<
 		"tail -$((entryCount)) " << trainFile << "  | sed -e 's/,/ /g' > data.tmp " << endl <<
 		"mkdir attrTrain " << endl <<
 		"for ((i=1; i <= $attrCount; i++)) " << endl <<
@@ -34,7 +34,7 @@ void dataPrep(string const &trainFile, string const &testFile)
 		"cat data2.tmp | awk '{print $var}' var=$j > attrTest/$j " << endl <<
 		"done";
 	fout.close();
-	system("chmod +x temp.sh");
+    system("chmod a+x ./temp.sh")
 	system("./temp.sh");
 }
 #endif
---
+++ src/Source.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cmath>
+#include <cstdlib>
+#include "DataSet.hpp"
+#include "DataPrep.hpp"
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    string trainFile(argv[1]);
+    string testFile(argv[2]);
+    bool isTAN = argv[3][0] == 't' || argv[3][0] == 'T';
+    
+    
+    
+    return 0;  
+}
